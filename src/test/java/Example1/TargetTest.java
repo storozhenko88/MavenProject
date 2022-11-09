d0ea4ddbdd17e67bc6ef8d51bb7895fdae3726ce
storozhenko <storozhenko-88@ukr.net>
2022-11-09 22:10:54 +0300

    Added package Example1

---
+++ src/test/java/Example1/TargetTest.java
@@ -0,0 +1,16 @@
+package Example1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TargetTest {
+
+    @Test
+    public void getIndexByTargetTest (){
+        Target target = new Target();
+        int[] numbers = {2,7,11,15};
+        int[] index = {1,2};
+
+        Assert.assertArrayEquals(index, target.getIndexByTarget(numbers, 18));
+    }
+}
