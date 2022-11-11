63916e729548d24b263cafbc273f1b51307f24a3
storozhenko <storozhenko-88@ukr.net>
2022-11-11 22:59:46 +0300

    Added package PascalTriangle

---
+++ src/test/java/pascal_triangle/PascalTriangleTest.java
@@ -0,0 +1,25 @@
+package pascal_triangle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PascalTriangleTest {
+
+
+    @Test
+    public void creatPascalTriTest (){
+        int rowNumber = 5;
+        PascalTriangle triangle = new PascalTriangle();
+        List<List<Integer>> valuesTriangle = Arrays.asList(
+                List.of(1),
+                List.of(1,1),
+                List.of(1,2,1),
+                List.of(1,3,3,1),
+                List.of(1,4,6,4,1));
+
+        Assert.assertEquals(valuesTriangle, triangle.creatPascalTri(rowNumber));
+    }
+}
